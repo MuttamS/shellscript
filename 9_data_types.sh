4299ac537b4e26299d21d59ab08d90d44a01a19d
Ravi <muttamsettyravichandrudu@gmail.com>
2025-10-23 00:56:52 +0530

    variables

--- 9_data_types.sh
+++ 9_data_types.sh
@@ -4,11 +4,10 @@
 NUMBER1=100
 NUMBER2=200
 
-SUM=$(($NUMBER1 + $NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Sum is: ${SUM}"
 
 LEADERS=("Modi" "Puthin" "Trudo" "Trump")
 
 echo "All Leaders: ${LEADERS[@]}"
-echo "All
