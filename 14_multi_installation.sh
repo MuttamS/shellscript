b1a79d4d8c157825aa61e1c05b6cfe92f08e5d1e
Ravi <muttamsettyravichandrudu@gmail.com>
2025-10-24 20:11:58 +0530

    condiations

--- 14_multi_installation.sh
+++ 14_multi_installation.sh
@@ -7,27 +7,42 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
+# dnf install mysql -y
+
+# if [ $? -ne 0 ]; then 
+#     echo "ERROR:: Installing mysql is failure"
+# else
+#     echo "Installing mysql is seccuss"
+# fi
+
+# dnf install nginx -y
+
+# if [ $? -ne 0 ]; then 
+#     echo "ERROR:: Installing nginx is failure"
+# else
+#     echo "Installing nginx is seccuss"
+# fi
+
+# dnf install python3 -y
+
+# if [ $? -ne 0 ]; then 
+#     echo "ERROR:: Installing python3 is failure"
+# else
+#     echo "Installing python3 is seccuss"
+# fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is Success"
+    fi
+}
+
 dnf install mysql -y
-
-if [ $? -ne 0 ]; then 
-    echo "ERROR:: Installing mysql is failure"
-else
-    echo "Installing mysql is seccuss"
-fi
-
+VALIDATE $? "mysql"
 dnf install nginx -y
-
-if [ $? -ne 0 ]; then 
-    echo "ERROR:: Installing nginx is failure"
-else
-    echo "Installing nginx is seccuss"
-fi
-
+VALIDATE $? "nginx"
 dnf install python3 -y
-
-if [ $? -ne 0 ]; then 
-    echo "ERROR:: Installing python3 is failure"
-else
-    echo "Installing python3 is seccuss"
-fi
-
+VALIDATE $? "python3"
