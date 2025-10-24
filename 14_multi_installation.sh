4f65004c03aa4c1277410e938ffbd5cec9703b53
Ravi <muttamsettyravichandrudu@gmail.com>
2025-10-24 19:26:08 +0530

    condiations

---
+++ 14_multi_installation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root previalges"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo "ERROR:: Installing mysql is failure"
+else
+    echo "Installing mysql is seccuss"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then 
+    echo "ERROR:: Installing nginx is failure"
+else
+    echo "Installing nginx is seccuss"
+fi
+
+dnf install mongodb-mongosh -y
+
+if [ $? -ne 0 ]; then 
+    echo "ERROR:: Installing ongodb-mongosh is failure"
+else
+    echo "Installing ongodb-mongosh is seccuss"
+fi
+
