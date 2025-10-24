b48313403403bd3dcea1c26257e23a198e3dbf24
Ravi <muttamsettyravichandrudu@gmail.com>
2025-10-24 18:00:59 +0530

    condiations

--- 12_condition.sh
+++ 12_condition.sh
@@ -3,7 +3,7 @@
 echo "please enter the number"
 read NUMBER
 
-if [ $NUMBER -lt 2 ]; then
+if [ $NUMBER -lt 1 ]; then
     echo "Given number $NUMBER is not prime number"
 else
     echo "Given number $NUMBER is prime number"
