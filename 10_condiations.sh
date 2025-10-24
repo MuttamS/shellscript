04e8bce814f57bcf2b2ce28067e1dfd08b1e7556
Ravi <muttamsettyravichandrudu@gmail.com>
2025-10-24 17:28:00 +0530

    condiations

--- 10_condiations.sh
+++ 10_condiations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NUMBER=$6
+NUMBER=$1
 
-if [ $NUMBER-gt 10 ]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is less then 10"
 else
-    echo "Given number $NUMBER is greater then or equal to 10"
+    echo "Given number $NUMBER is grater then or equal to 10"
 fi
